dd10c2f6da1a87678f3b63d3d70d68038971e007
ChristosPetrou <c.petrou@tiomarkets.com>
2023-08-24 16:12:31 +0300

    init projecty

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import PageLayout from "@/components/PageLayout";
+import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
+  );
 }
